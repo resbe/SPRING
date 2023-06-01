f54a5eef6764dc18d5ffaa23506205da8de651fd
user <user@DESKTOP-G84AIPR>
2023-06-01 17:55:17 +0900

    0601

---
+++ ex230601/src/main/java/com/yedam/app/emp/service/EmpVO.java
@@ -0,0 +1,10 @@
+package com.yedam.app.emp.service;
+
+import lombok.Data;
+
+@Data
+public class EmpVO {
+	String employee_id;
+	String first_name;
+	String last_name;
+}	
---
+++ ex230601/src/test/java/com/yedam/app/EmpMapperClient.java
@@ -0,0 +1,27 @@
+package com.yedam.app;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.yedam.app.emp.mapper.EmpMapper;
+import com.yedam.app.emp.service.EmpVO;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "file:src/main/webapp/WEB-INF/spring/root-context.xml")
+public class EmpMapperClient {
+	@Autowired
+	EmpMapper empMapper;
+	
+	@Test
+	public void getEmpInfo() {
+		EmpVO findEmp = empMapper.selectOne(100);
+		
+		//assertEquals = 두개를 비교해서 같은지 확인 
+		assertEquals(findEmp.getFirst_name(), "Steven");
+	}
+}
